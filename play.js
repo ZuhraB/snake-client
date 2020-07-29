6bcb94d47e25af7ffa0c903506ab01abbfa49e2a
ZuhraB <zuhra.bakhshi.com>
2020-07-29 20:31:31 +0000

    snake

--- play.js
+++ play.js
@@ -1,17 +1,8 @@
 const { connect } = require('./client');
+const {setupInput} = require('./input')
 console.log('Connecting ...');
 connect();
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
-const handleUserInput = function(data){
-  if (setupInput) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  }
-}
+
+
+setupInput();
+
